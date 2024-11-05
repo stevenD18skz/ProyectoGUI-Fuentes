f1f90f012d1d987f3d2259729c62e0fe828fb3ef
Brayan Steven Narvez Valdes <brayan.steven.narvaez@correounivalle.edu.co>
2024-11-04 23:19:29 -0500

    oraganizar

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from gui import *
+#from gui import *
 
 
+m = 3
+opiniones_nuevas = [1, 3, 2]
+valores = [3, 6, 1]
 
+for i in range(3):
+    for j in range(opiniones_nuevas[i]):
+        print(valores[i])
+
+
+"""
 if __name__ == '__main__':
     gui = GUI()
     gui.run()
+"""
